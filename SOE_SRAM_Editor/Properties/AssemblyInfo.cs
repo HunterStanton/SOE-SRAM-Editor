513bd8c9b9bb20437d6c7507532e6f42d3acc7e1
Hunter Stanton <ihatecompvir@icloud.com>
2017-02-16 23:37:42 -0500

    Updated assembly info

--- SOE_SRAM_Editor/Properties/AssemblyInfo.cs
+++ SOE_SRAM_Editor/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SOE_SRAM_Editor")]
+[assembly: AssemblyTitle("Secret of Evermore SRAM Editor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SOE_SRAM_Editor")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Secret of Evermore SRAM Editor")]
+[assembly: AssemblyCopyright("Copyright © Hunter Stanton 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
