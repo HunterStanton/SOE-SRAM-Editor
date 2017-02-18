8f797d0dce20f7b9e9ac486ce60ca5f0a11c695c
Hunter Stanton <ihatecompvir@icloud.com>
2017-02-18 03:29:06 -0500

    SRAM class coming together

--- SOE_SRAM_Editor/Classes/SRAM.cs
+++ SOE_SRAM_Editor/Classes/SRAM.cs
@@ -13,5 +13,188 @@ namespace SOE_SRAM_Editor
     /// </summary>
     class SRAM
     {
+        // The 4 Save slots that can exist in an SRAM file
+        public Save[] slots = new Save[4];
+
+        /// <summary>
+        /// Represents a single save slot
+        /// </summary>
+        public class Save
+        {
+            public Boy boy = new Boy();
+            public Dog dog = new Dog();
+            public Money money = new Money();
+            public Weapons weapons = new Weapons();
+            public Ingredients ingredients = new Ingredients();
+            public Items items = new Items();
+            public Armor armor = new Armor();
+            public Helmet helmet = new Helmet();
+        }
+
+        /// <summary>
+        /// The Boys statistics
+        /// </summary>
+        public class Boy
+        {
+            public string Name;
+            public int Level;
+            public int Experience;
+            public int HP; // HP at the time of the save
+        }
+
+        /// <summary>
+        /// The Dogs statistics
+        /// </summary>
+        public class Dog
+        {
+            public string Name;
+            public int Level;
+            public int Experience;
+            public int HP; // HP at the time of the save
+
+            // Only Dog has this because Dog doesn't have switchable weapons
+            public int AttackLevel;
+            public int AttackXP; // How close the attack level is to the next level, values 0-99
+        }
+
+        /// <summary>
+        /// The types of money that are possible to receive
+        /// </summary>
+        public class Money
+        {
+            public int Talons; // Prehistoria
+            public int Jewels; // Antiqua
+            public int Coins; // Gothica
+            public int Credits; // Omnitopia
+        }
+        
+        /// <summary>
+        /// Weapons the boy can wield
+        /// </summary>
+        public class Weapons
+        {
+            public bool HasBoneCrusher;
+            public int BoneCrusherLevel;
+            public int BoneCrusherXP;
+            public bool HasSpidersClaw;
+            public int SpidersClawLevel;
+            public int SpidersClawXP;
+            public bool HasHornedSpear;
+            public int HornedSpearLevel;
+            public int HornedSpearXP;
+            public bool HasGladiatorSword;
+            public int GladiatorSwordLevel;
+            public int GladiatorSwordXP;
+            public bool HasBronzeAxe;
+            public int BronzeAxeLevel;
+            public int BronzeAxeXP;
+            public bool HasBronzeSpear;
+            public int BronzeSpearLevel;
+            public int BronzeSpearXP;
+            public bool HasCrusaderSword;
+            public int CrusaderSwordLevel;
+            public int CrusaderSwordXP;
+            public bool HasKnightBasher;
+            public int KnightBasherLevel;
+            public int KnightBasherXP;
+            public bool HasLance;
+            public int LanceLevel;
+            public int LanceXP;
+            public bool HasNeutronBlade;
+            public int NeutronBladeLevel;
+            public int NeutronBladeXP;
+            public bool HasAtomSmasher;
+            public int AtomSmasherLevel;
+            public int AtomSmasherXP;
+            public bool HasLaserLance;
+            public int LaserLanceLevel;
+            public int LaserLanceXP;
+            public bool HasBazooka;
+            public int ThunderBallAmmo;
+            public int ParticleBombAmmo;
+            public int CryoBlastAmmo;
+        }
+
+        /// <summary>
+        /// Alchemy ingredients
+        /// </summary>
+        public class Ingredients
+        {
+            public int Acorn;
+            public int Ash;
+            public int AtlasMedallion;
+            public int Bone;
+            public int Brimstone;
+            public int Clay;
+            public int Crystal;
+            public int DryIce;
+            public int Ethanol;
+            public int Feather;
+            public int Grease;
+            public int Gunpowder;
+            public int Iron;
+            public int Limestone;
+            public int Meteorite;
+            public int MudPepper;
+            public int Mushroom;
+            public int Oil;
+            public int Root;
+            public int Vinegar;
+            public int Water;
+            public int Wax;
+        }
+
+        /// <summary>
+        /// Consumable items that can be bought/found/dropped. Max 6 normally, but can be forced higher.
+        /// </summary>
+        public class Items
+        {
+            public int Petal;
+            public int Nectar;
+            public int Honey;
+            public int DogBiscuit;
+            public int Wings;
+            public int Essence;
+            public int PixieDust;
+            public int CallBeads;
+        }
+
+        /// <summary>
+        /// Chest piece items that boost defense.
+        /// </summary>
+        public class Armor
+        {
+            public int GrassVest;
+            public int ShellPlate;
+            public int DinoSkin;
+            public int BronzeArmor;
+            public int StoneVest;
+            public int CenturionCape;
+            public int SilverMail;
+            public int GoldPlatedVest;
+            public int ShiningArmor;
+            public int MagnaMail;
+            public int TitaniumVest;
+            public int VirtualVest;
+        }
+
+        /// <summary>
+        /// Helmet piece items that boost defense.
+        /// </summary>
+        public class Helmet
+        {
+            public int GrassHat;
+            public int ShellHat;
+            public int DinoHelm;
+            public int BronzeHelmet;
+            public int ObsidianHelm;
+            public int CenturionHelm;
+            public int TitansCrown;
+            public int DragonHelm;
+            public int KnightsHelm;
+            public int LightningHelm;
+            public int OldReliable;
+            public int Brainstorm;
+        }
     }
 }
