899936ccf77bae475328e36f840ebb5cb3a35cb0
Hunter Stanton <ihatecompvir@icloud.com>
2017-02-18 03:01:59 -0500

    Added SRAM class

---
+++ SOE_SRAM_Editor/Classes/SRAM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOE_SRAM_Editor
+{
+    /// <summary>
+    /// Class that represents the SRAM.
+    /// This will get returned by the LoadSRAM class to the main window, where the UI will be populated with values from it.
+    /// Then, when the user saves, it will take those values from UI, save them, and then commit them to file.
+    /// </summary>
+    class SRAM
+    {
+    }
+}
