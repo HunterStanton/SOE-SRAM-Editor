4119a02a4960a8908c22ea55e70f6f8d29794a63
Hunter Stanton <ihatecompvir@icloud.com>
2017-02-18 00:16:35 -0500

    Added Sanity class stub

---
+++ SOE_SRAM_Editor/Classes/Sanity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOE_SRAM_Editor
+{
+    /// <summary>
+    /// A class that allows for recalcualting the sanity check/checksum in the SRAM.
+    /// Written using the super helpful guide at http://games.technoplaza.net/soesrame/sram-doc.txt
+    /// This has the original 65c816 assembly as well as C++ code for recalculating it.
+    /// I essentially rewrote the C++ code into C# and converted it into a class I could use at will.
+    /// </summary>
+    class Sanity
+    {
+    }
+}
