7c0925581f614bc8e3c116b4485c4bf317a7df99
Hunter Stanton <ihatecompvir@icloud.com>
2017-02-18 02:59:50 -0500

    Added LoadSRAM class

---
+++ SOE_SRAM_Editor/Classes/LoadSRAM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOE_SRAM_Editor
+{
+    /// <summary>
+    /// Handles loading the SRAM file and grabbing information from it.
+    /// </summary>
+    class LoadSRAM
+    {
+    }
+}
